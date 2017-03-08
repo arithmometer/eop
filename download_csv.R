e42f3b778a28e22f3a0756488596c78035aa1901
root <root@arithmometer.localdomain>
2017-03-08 00:33:40 +0000

    Fix mjd to be the starting point of forecast

--- download_csv.R
+++ download_csv.R
@@ -1,3 +1,5 @@
+prefix <- "/srv/shiny-server/eop/"
+
 # urls
 url_finals2000a <- "https://datacenter.iers.org/eop/-/somos/5Rgv/latestXL/13/finals2000A.daily/csv"
 url_finals_daily <- "https://datacenter.iers.org/eop/-/somos/5Rgv/latestXL/12/finals.daily/csv"
@@ -10,12 +12,8 @@ date.string <- format(Sys.time(), "%Y-%m-%d")
 # since midnight on November 17, 1858. For example, 51544.0 in MJD is midnight on January 1, 2000.
 mjd.string <- as.character(as.Date(date.string) - as.Date("1858-11-17"))
 
-if(!dir.exists("csv")) {
-   dir.create("csv")
-}
+dir.create(paste(prefix, "csv/", mjd.string, sep=""))
 
-dir.create(paste("csv/", mjd.string, sep=""))
-
-download.file(url_finals2000a, paste("csv/", date.string, "/finals2000A.daily.csv", sep=""), method="auto", quiet=TRUE)
-download.file(url_finals_daily, paste("csv/", date.string, "/finals.daily.csv", sep=""), method="auto", quiet=TRUE)
-download.file(url_gpsrapid, paste("csv/", date.string, "/gpsrapid.daily.csv", sep=""), method="auto", quiet=TRUE)
+download.file(url_finals2000a, paste(prefix, "csv/", mjd.string, "/finals2000A.daily.csv", sep=""), method="auto", quiet=TRUE)
+download.file(url_finals_daily, paste(prefix, "csv/", mjd.string, "/finals.daily.csv", sep=""), method="auto", quiet=TRUE)
+download.file(url_gpsrapid, paste(prefix, "csv/", mjd.string, "/gpsrapid.daily.csv", sep=""), method="auto", quiet=TRUE)
