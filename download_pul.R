53c7444b3c3912771b3ed250ccae492e5d00055b
Grigory Okhotnikov <grishk@ro.ru>
2017-03-09 12:54:13 +0300

    Fix download Pulkovo forecasts lag

--- download_pul.R
+++ download_pul.R
@@ -8,10 +8,9 @@ get_file <- function(filename, url) {
 }
 
 mjd <- as.integer(as.Date(date.string) - as.Date("1858-11-17"))
-lag <- 2
 
-am.filename <- paste(mjd - 1 - lag, "_am_pul.txt", sep="")
-e1.filename <- paste(mjd - 1 - lag, "_e1_pul.txt", sep="")
+am.filename <- paste(mjd - 1, "_am_pul.txt", sep="")
+e1.filename <- paste(mjd - 1, "_e1_pul.txt", sep="")
 
 get_file(paste("pul/", am.filename, sep=""), paste("http://www.gao.spb.ru/english/as/persac/eopcppp/", am.filename, sep=""))
 get_file(paste("pul/", e1.filename, sep=""), paste("http://www.gao.spb.ru/english/as/persac/eopcppp/", e1.filename, sep=""))
