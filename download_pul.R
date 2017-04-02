c1d3816e26eb19276ab9d9e534db82e00f7863e8
Grigory Okhotnikov <grishk@ro.ru>
2017-04-02 19:10:40 +0300

    Fix download of Pulkovo files

--- download_pul.R
+++ download_pul.R
@@ -1,17 +1,24 @@
 get_file <- function(filename, url) {
-  if(!file.exists(filename)) {
-    download.file(url, filename, method = "auto", quiet=TRUE)
-    if(!file.exists(filename)) {
-      stop(paste(filename, " file does not exist"), call.=FALSE)
-    }
-  }
+  tryCatch({download.file(url, destfile=filename)}, silent = TRUE, condition = function(err) { } )
 }
+
+last.filename <- "pul/last.csv"
+last <- read.csv(last.filename)
+newlast <- last
 
 date.string <- format(Sys.time(), "%Y-%m-%d")
 mjd <- as.integer(as.Date(date.string) - as.Date("1858-11-17"))
 
-am.filename <- paste(mjd - 1, "_am_pul.txt", sep="")
-e1.filename <- paste(mjd - 1, "_e1_pul.txt", sep="")
+for(day in (last$x + 1):mjd) {
+  am.filename <- paste(day, "_am_pul.txt", sep="")
+  e1.filename <- paste(day, "_e1_pul.txt", sep="")
+  
+  get_file(paste("pul/", am.filename, sep=""), paste("http://www.gao.spb.ru/english/as/persac/eopcppp/", am.filename, sep=""))
+  get_file(paste("pul/", e1.filename, sep=""), paste("http://www.gao.spb.ru/english/as/persac/eopcppp/", e1.filename, sep=""))
+  
+  if(file.exists(paste("pul/", am.filename, sep="")) || file.exists(paste("pul/", e1.filename, sep=""))) {
+    newlast <- day
+  }
+}
 
-get_file(paste("pul/", am.filename, sep=""), paste("http://www.gao.spb.ru/english/as/persac/eopcppp/", am.filename, sep=""))
-get_file(paste("pul/", e1.filename, sep=""), paste("http://www.gao.spb.ru/english/as/persac/eopcppp/", e1.filename, sep=""))
+write.csv(newlast, last.filename)
