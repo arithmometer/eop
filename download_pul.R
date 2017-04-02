7cfce159067149cbaa907956b0e23d8e19ab637d
Grigory Okhotnikov <grishk@ro.ru>
2017-04-02 19:28:31 +0300

    Fix Pulkovo files download

--- download_pul.R
+++ download_pul.R
@@ -10,13 +10,16 @@ date.string <- format(Sys.time(), "%Y-%m-%d")
 mjd <- as.integer(as.Date(date.string) - as.Date("1858-11-17"))
 
 for(day in (last$x + 1):mjd) {
-  am.filename <- paste(day, "_am_pul.txt", sep="")
-  e1.filename <- paste(day, "_e1_pul.txt", sep="")
+  am.name <- paste(day, "_am_pul.txt", sep="")
+  e1.name <- paste(day, "_e1_pul.txt", sep="")
   
-  get_file(paste("pul/", am.filename, sep=""), paste("http://www.gao.spb.ru/english/as/persac/eopcppp/", am.filename, sep=""))
-  get_file(paste("pul/", e1.filename, sep=""), paste("http://www.gao.spb.ru/english/as/persac/eopcppp/", e1.filename, sep=""))
+  am.filename <- paste("pul/", am.name, sep="")
+  e1.filename <- paste("pul/", e1.name, sep="")
   
-  if(file.exists(paste("pul/", am.filename, sep="")) || file.exists(paste("pul/", e1.filename, sep=""))) {
+  get_file(am.filename, paste("http://www.gao.spb.ru/english/as/persac/eopcppp/", am.name, sep=""))
+  get_file(e1.filename, paste("http://www.gao.spb.ru/english/as/persac/eopcppp/", e1.name, sep=""))
+  
+  if(file.info(am.filename)$size > 0 || file.info(e1.filename)$size > 0) {
     newlast <- day
   }
 }
