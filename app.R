1523f5bdefb19d629c9c6754f44fd09e0bcd1842
Grigory Okhotnikov <grishk@ro.ru>
2017-04-03 03:38:49 +0300

    Add some more code folding

--- app.R
+++ app.R
@@ -30,17 +30,6 @@ server <- function(input, output, clientData, session) {
     },
     
     content <- function(file) {
-      file.copy(paste(forecast.mjd(), "_ssa_spbu_365.txt", sep=""), file)
-    },
-    contentType = "application/txt"
-  )
-  
-  output$downloadForecast365 <- downloadHandler(
-    filename <- function() {
-      paste(forecast.mjd(), "_ssa_spbu_365.txt", sep="")
-    },
-    
-    content <- function(file) {
       file.copy(paste("ssa/", forecast.mjd(), "_ssa_spbu_365.txt", sep=""), file)
     },
     contentType = "text/plain"
@@ -63,13 +52,13 @@ server <- function(input, output, clientData, session) {
     ssa.forecast
   })
   
-  get_forecast_mjd <- reactive({
+  get_compare_mjd <- reactive({
     mjd <- as.integer(as.Date(input$date_compare) - as.Date("1858-11-17"))
     mjd
   })
   
   get_ssa <- reactive({
-    mjd <- get_forecast_mjd()
+    mjd <- get_compare_mjd()
     ssa.forecast <- read.table(paste("ssa/", mjd, "_ssa_spbu_365.txt", sep="")) # ssa/55434_ssa_spbu_365.txt
     colnames(ssa.forecast) <- c("MJD", "x", "y", "LOD", "dX", "dY")
     ssa.forecast
@@ -77,7 +66,7 @@ server <- function(input, output, clientData, session) {
 
   # check if these files do exist!
   get_pul_am <- reactive({
-    mjd <- get_forecast_mjd()
+    mjd <- get_compare_mjd()
     am_pul <- read.table(paste("pul/", mjd - 1, "_am_pul.txt", sep = ""), skip=1) # pul/55434_am_pul.txt
     colnames(am_pul) <- c("MJD", "x", "y", "TAI-UT1", "LOD", "dX", "dY")
     am_pul$dX <- am_pul$dX * 10**(-3)
@@ -87,7 +76,7 @@ server <- function(input, output, clientData, session) {
   })
 
   get_pul_e1 <- reactive({
-    mjd <- get_forecast_mjd()
+    mjd <- get_compare_mjd()
     e1_pul <- read.table(paste("pul/", mjd - 1, "_e1_pul.txt", sep = ""), skip=1) # pul/55434_am_pul.txt
     colnames(e1_pul) <- c("MJD", "x", "y", "TAI-UT1", "LOD", "dX", "dY")
     e1_pul$dX <- e1_pul$dX * 10**(-3)
@@ -97,7 +86,7 @@ server <- function(input, output, clientData, session) {
   })
 
   get_final <- reactive({
-    mjd <- get_forecast_mjd()
+    mjd <- get_compare_mjd()
     c04.file <- "eopc04_IAU2000.62-now.txt"
     c04 <- read.table(c04.file, comment.char = "#", skip = 14)
     colnames(c04) <- c("Year", "Month", "Day", "MJD", "x", "y", "UT1-UTC",
@@ -105,142 +94,37 @@ server <- function(input, output, clientData, session) {
     c04
   })
   
-  output$x_today <- renderPlotly({
-    if(input$"mjd_labels") {
-      lab <- "MJD"
-      ticks <- forecast.mjd():(forecast.mjd()+364)
-    } else {
-      lab <- "Date"
-      start.date <- mjd_to_date(forecast.mjd())
-      tm <- seq(0, 364, by = 1)
-      ticks <- start.date + tm
-    }
-    
-    plot_ly(get_ssa_today(), y=~x, x=~ticks, type="scatter", mode="markers") %>% 
-      layout(xaxis=list(title=lab)) %>% 
-      layout(yaxis=list(title="x"))
-  })
-  
-  output$y_today <- renderPlotly({
-    if(input$"mjd_labels") {
-      lab <- "MJD"
-      ticks <- forecast.mjd():(forecast.mjd()+364)
-    } else {
-      lab <- "Date"
-      start.date <- mjd_to_date(forecast.mjd())
-      tm <- seq(0, 364, by = 1)
-      ticks <- start.date + tm
-    }
-    
-    plot_ly(get_ssa_today(), y=~y, x=~ticks, type="scatter", mode="markers") %>% 
-      layout(xaxis=list(title=lab)) %>% 
-      layout(yaxis=list(title="y"))
-  })
-  
-  output$LOD_today <- renderPlotly({
-    if(input$"mjd_labels") {
-      lab <- "MJD"
-      ticks <- forecast.mjd():(forecast.mjd()+364)
-    } else {
-      lab <- "Date"
-      start.date <- mjd_to_date(forecast.mjd())
-      tm <- seq(0, 364, by = 1)
-      ticks <- start.date + tm
-    }
-    
-    plot_ly(get_ssa_today(), y=~LOD, x=~ticks, type="scatter", mode="markers") %>% 
-      layout(xaxis=list(title=lab)) %>% 
-      layout(yaxis=list(title="LOD"))
-  })
-  
-  output$dX_today <- renderPlotly({
-    if(input$"mjd_labels") {
-      lab <- "MJD"
-      ticks <- forecast.mjd():(forecast.mjd()+364)
-    } else {
-      lab <- "Date"
-      start.date <- mjd_to_date(forecast.mjd())
-      tm <- seq(0, 364, by = 1)
-      ticks <- start.date + tm
-    }
-    
-    plot_ly(get_ssa_today(), y=~dX, x=~ticks, type="scatter", mode="markers") %>% 
-      layout(xaxis=list(title=lab)) %>% 
-      layout(yaxis=list(title="dX"))
-  })
-  
-  output$dY_today <- renderPlotly({
-    if(input$"mjd_labels") {
-      lab <- "MJD"
-      ticks <- forecast.mjd():(forecast.mjd()+364)
-    } else {
-      lab <- "Date"
-      start.date <- mjd_to_date(forecast.mjd())
-      tm <- seq(0, 364, by = 1)
-      ticks <- start.date + tm
-    }
-    
-    plot_ly(get_ssa_today(), y=~dY, x=~ticks, type="scatter", mode="markers") %>% 
-      layout(xaxis=list(title=lab)) %>% 
-      layout(yaxis=list(title="dY"))
+  eop.list <- list("x", "y", "LOD", "dX", "dY")
+  lapply(eop.list, function(eop) {
+    output[[paste(eop, "_today", sep="")]] <- renderPlotly({
+      if(input$"mjd_labels") {
+        lab <- "MJD"
+        ticks <- forecast.mjd():(forecast.mjd()+364)
+      } else {
+        lab <- "Date"
+        start.date <- mjd_to_date(forecast.mjd())
+        tm <- seq(0, 364, by = 1)
+        ticks <- start.date + tm
+      }
+      
+      plot_ly(y=get_ssa_today()[[eop]], x=~ticks, type="scatter", mode="lines") %>% 
+        layout(xaxis=list(title=lab)) %>% 
+        layout(yaxis=list(title=eop))
+    })  
   })
 
-  output$x_dists <- renderUI({
-    L_list <- read.csv("today/x_365_L_list.csv")
-    do.call(tabsetPanel, lapply(L_list$x, function(L) {
-      dists <- read.csv(paste("today/x_365_", L, "_dists.csv", sep=""))
-      output[[paste("x_dists_365_", L, sep="")]] <- renderPlotly(plot_ly(dists, y=~x, x=~X, type="scatter", mode="markers") %>%
-                                                                   layout(xaxis=list(title="Number of components")) %>% 
-                                                                   layout(yaxis=list(title="MSE")))
-      tabPanel(paste("L = ", L, sep=""), plotlyOutput(paste("x_dists_365_", L, sep="")))
-    }))
-  })
-  
-  output$y_dists <- renderUI({
-    L_list <- read.csv("today/y_365_L_list.csv")
-    do.call(tabsetPanel, lapply(L_list$x, function(L) {
-      dists <- read.csv(paste("today/y_365_", L, "_dists.csv", sep=""))
-      output[[paste("y_dists_365_", L, sep="")]] <- renderPlotly(plot_ly(dists, y=~x, x=~X, type="scatter", mode="markers") %>%
-                                                                   layout(xaxis=list(title="Number of components")) %>% 
-                                                                   layout(yaxis=list(title="MSE")))
-      tabPanel(paste("L = ", L, sep=""), plotlyOutput(paste("y_dists_365_", L, sep="")))
-    }))
-  })
-  
-  output$LOD_dists <- renderUI({
-    L_list <- read.csv("today/LOD_365_L_list.csv")
-    do.call(tabsetPanel, lapply(L_list$x, function(L) {
-      dists <- read.csv(paste("today/LOD_365_", L, "_dists.csv", sep=""))
-      output[[paste("LOD_dists_365_", L, sep="")]] <- renderPlotly(plot_ly(dists, y=~x, x=~X, type="scatter", mode="markers") %>%
-                                                                   layout(xaxis=list(title="Number of components")) %>% 
-                                                                   layout(yaxis=list(title="MSE")))
-      tabPanel(paste("L = ", L, sep=""), plotlyOutput(paste("LOD_dists_365_", L, sep="")))
-    }))
-  })
-  
-  output$dX_dists <- renderUI({
-    L_list <- read.csv("today/dX_365_L_list.csv")
-    do.call(tabsetPanel, lapply(L_list$x, function(L) {
-      dists <- read.csv(paste("today/dX_365_", L, "_dists.csv", sep=""))
-      output[[paste("dX_dists_365_", L, sep="")]] <- renderPlotly(plot_ly(dists, y=~x, x=~X, type="scatter", mode="markers") %>%
+  lapply(eop.list, function(eop) {
+    output[[paste(eop, "_dists", sep="")]] <- renderUI({
+      L_list <- read.csv(paste("today/", eop, "_365_L_list.csv", sep=""))
+      do.call(tabsetPanel, lapply(L_list$x, function(L) {
+        dists <- read.csv(paste("today/", eop, "_365_", L, "_dists.csv", sep=""))
+        output[[paste(eop, "_dists_365_", L, sep="")]] <- renderPlotly(plot_ly(dists, y=~x, x=~X, type="scatter", mode="markers") %>%
                                                                      layout(xaxis=list(title="Number of components")) %>% 
                                                                      layout(yaxis=list(title="MSE")))
-      tabPanel(paste("L = ", L, sep=""), plotlyOutput(paste("dX_dists_365_", L, sep="")))
-    }))
+        tabPanel(paste("L = ", L, sep=""), plotlyOutput(paste(eop, "_dists_365_", L, sep="")))
+      }))
+    })
   })
-  
-  output$dY_dists <- renderUI({
-    L_list <- read.csv("today/dY_365_L_list.csv")
-    do.call(tabsetPanel, lapply(L_list$x, function(L) {
-      dists <- read.csv(paste("today/dY_365_", L, "_dists.csv", sep=""))
-      output[[paste("dY_dists_365_", L, sep="")]] <- renderPlotly(plot_ly(dists, y=~x, x=~X, type="scatter", mode="markers") %>%
-                                                                    layout(xaxis=list(title="Number of components")) %>% 
-                                                                    layout(yaxis=list(title="MSE")))
-      tabPanel(paste("L = ", L, sep=""), plotlyOutput(paste("dY_dists_365_", L, sep="")))
-    }))
-  })
-  
-  eop.list <- list("x", "y", "LOD", "dX", "dY")
   
   output$tabset_eop <- renderUI({
     do.call(tabsetPanel, lapply(eop.list, function(eop) {
@@ -256,137 +140,45 @@ server <- function(input, output, clientData, session) {
     }))
   })
   
-  get_params_today <- reactive({
+  get_params_today_365 <- reactive({
     params <- read.csv("today/365params.csv")
     params
   })
   
-  output$x_params_365 <- reactive({
-    params <- get_params_today()
-    sprintf("L: %d\np: %d", params$x[1], params$x[2])
-  })
-  
-  output$y_params_365 <- reactive({
-    params <- get_params_today()
-    sprintf("L: %d\np: %d", params$y[1], params$y[2])
-  })
-  
-  output$lod_params_365 <- reactive({
-    params <- get_params_today()
-    sprintf("L: %d\np: %d", params$LOD[1], params$LOD[2])
-  })
-  
-  output$dx_params_365 <- reactive({
-    params <- get_params_today()
-    sprintf("L: %d\np: %d", params$dX[1], params$dX[2])
-  })
-  
-  output$dy_params_365 <- reactive({
-    params <- get_params_today()
-    sprintf("L: %d\np: %d", params$dY[1], params$dY[2])
+  lapply(eop.list, function(eop) {
+    output[[paste(eop, "_params_365", sep="")]] <- reactive({
+      params <- get_params_today_365()
+      sprintf("L: %d\np: %d", params[[eop]][1], params[[eop]][2])
+    })
   })
   
   legend.names <- c("C04", "SSA", "Pul AM", "Pul E1")
   legend.colors <- c("black", "blue", "orange", "green")
   
-  output$x_comparison <- renderPlot({
-    chosen <- c(TRUE, FALSE, FALSE, FALSE)
-    mjd <- get_forecast_mjd()
-    ind <- mjd - 37664
-    plot(mjd:(mjd+364), get_final()[(ind):(ind + 364), "x"], type="l", xlab="MJD", ylab="Pole x")
-    if("ssa" %in% input$displaySeries) {
-      chosen[2] <- TRUE
-      lines(mjd:(mjd+364), get_ssa()[1:365, "x"], col="blue", lwd=2)
-    }
-    if("pul_am" %in% input$displaySeries) {
-      chosen[3] <- TRUE
-      lines(mjd:(mjd+364), get_pul_am()[1:365, "x"], col="orange")
-    }
-    if("pul_e1" %in% input$displaySeries) {
-      chosen[4] <- TRUE
-      lines(mjd:(mjd+364), get_pul_e1()[1:365, "x"], col="green")
-    }
-    legend("topright", legend.names[chosen], col=legend.colors[chosen], lty=1, bty='n', cex=1.25, xpd=TRUE)
-  })
+  series.list <- c("ssa", "pul_am", "pul_e1")
+  series.names <- c("SSA", "Pul AM", "Pul E1")
+  series.getters <- list(get_ssa, get_pul_am, get_pul_e1)
   
-  output$y_comparison <- renderPlot({
-    chosen <- c(TRUE, FALSE, FALSE, FALSE)
-    mjd <- get_forecast_mjd()
-    ind <- mjd - 37664
-    plot(mjd:(mjd+364), get_final()[(ind):(ind + 364), "y"], type="l", xlab="MJD", ylab="Pole y")
-    if("ssa" %in% input$displaySeries) {
-      chosen[2] <- TRUE
-      lines(mjd:(mjd+364), get_ssa()[1:365, "y"], col="blue", lwd=2)
-    }
-    if("pul_am" %in% input$displaySeries) {
-      chosen[3] <- TRUE
-      lines(mjd:(mjd+364), get_pul_am()[1:365, "y"], col="orange")
-    }
-    if("pul_e1" %in% input$displaySeries) {
-      chosen[4] <- TRUE
-      lines(mjd:(mjd+364), get_pul_e1()[1:365, "y"], col="green")
-    }
-    legend("topright", legend.names[chosen], col=legend.colors[chosen], lty=1, bty='n', cex=1.25, xpd=TRUE)
-  })
-  
-  output$lod_comparison <- renderPlot({
-    chosen <- c(TRUE, FALSE, FALSE, FALSE)
-    mjd <- get_forecast_mjd()
-    ind <- mjd - 37664
-    plot(mjd:(mjd+364), get_final()[(ind):(ind + 364), "LOD"], type="l", xlab="MJD", ylab="LOD")
-    if("ssa" %in% input$displaySeries) {
-      chosen[2] <- TRUE
-      lines(mjd:(mjd+364), get_ssa()[1:365, "LOD"], col="blue", lwd=2)
-    }
-    if("pul_am" %in% input$displaySeries) {
-      chosen[3] <- TRUE
-      lines(mjd:(mjd+364), get_pul_am()[1:365, "LOD"], col="orange")
-    }
-    if("pul_e1" %in% input$displaySeries) {
-      chosen[4] <- TRUE
-      lines(mjd:(mjd+364), get_pul_e1()[1:365, "LOD"], col="green")
-    }
-    legend("topright", legend.names[chosen], col=legend.colors[chosen], lty=1, bty='n', cex=1.25, xpd=TRUE)
-  })
-  
-  output$dx_comparison <- renderPlot({
-    chosen <- c(TRUE, FALSE, FALSE, FALSE)
-    mjd <- get_forecast_mjd()
-    ind <- mjd - 37664
-    plot(mjd:(mjd+364), get_final()[(ind):(ind + 364), "dX"], type="l", xlab="MJD", ylab="dX")
-    if("ssa" %in% input$displaySeries) {
-      chosen[2] <- TRUE
-      lines(mjd:(mjd+364), get_ssa()[1:365, "dX"], col="blue", lwd=2)
-    }
-    if("pul_am" %in% input$displaySeries) {
-      chosen[3] <- TRUE
-      lines(mjd:(mjd+364), get_pul_am()[1:365, "dX"], col="orange")
-    }
-    if("pul_e1" %in% input$displaySeries) {
-      chosen[4] <- TRUE
-      lines(mjd:(mjd+364), get_pul_e1()[1:365, "dX"], col="green")
-    }
-    legend("topright", legend.names[chosen], col=legend.colors[chosen], lty=1, bty='n', cex=1.25, xpd=TRUE)
-  })
-  
-  output$dy_comparison <- renderPlot({
-    chosen <- c(TRUE, FALSE, FALSE, FALSE)
-    mjd <- get_forecast_mjd()
-    ind <- mjd - 37664
-    plot(mjd:(mjd+364), get_final()[(ind):(ind + 364), "dY"], type="l", xlab="MJD", ylab="dY")
-    if("ssa" %in% input$displaySeries) {
-      chosen[2] <- TRUE
-      lines(mjd:(mjd+364), get_ssa()[1:365, "dY"], col="blue", lwd=2)
-    }
-    if("pul_am" %in% input$displaySeries) {
-      chosen[3] <- TRUE
-      lines(mjd:(mjd+364), get_pul_am()[1:365, "dY"], col="orange")
-    }
-    if("pul_e1" %in% input$displaySeries) {
-      chosen[4] <- TRUE
-      lines(mjd:(mjd+364), get_pul_e1()[1:365, "dY"], col="green")
-    }
-    legend("topright", legend.names[chosen], col=legend.colors[chosen], lty=1, bty='n', cex=1.25, xpd=TRUE)
+  lapply(eop.list, function(eop) {
+    output[[paste(eop, "_comparison", sep="")]] <- renderPlot({
+      chosen <- c(TRUE, FALSE, FALSE, FALSE)
+      mjd <- get_compare_mjd()
+      ind <- mjd - 37664
+      plot(mjd:(mjd+364), get_final()[(ind):(ind + 364), eop], type="l", xlab="MJD", ylab=eop)
+      if("ssa" %in% input$displaySeries) {
+        chosen[2] <- TRUE
+        lines(mjd:(mjd+364), get_ssa()[1:365, "x"], col="blue", lwd=2)
+      }
+      if("pul_am" %in% input$displaySeries) {
+        chosen[3] <- TRUE
+        lines(mjd:(mjd+364), get_pul_am()[1:365, "x"], col="orange")
+      }
+      if("pul_e1" %in% input$displaySeries) {
+        chosen[4] <- TRUE
+        lines(mjd:(mjd+364), get_pul_e1()[1:365, "x"], col="green")
+      }
+      legend("topright", legend.names[chosen], col=legend.colors[chosen], lty=1, bty='n', cex=1.25, xpd=TRUE)
+    })
   })
   
   MSE <- function(a, b, n) {
@@ -395,37 +187,20 @@ server <- function(input, output, clientData, session) {
   
   output$comparison_table <- renderTable({
     df <- data.frame(x=double(), y=double(), LOD=double(), dX=double(), dY=double())
-    mjd <- get_forecast_mjd()
+    mjd <- get_compare_mjd()
     ind <- mjd - 37664
-    if("ssa" %in% input$displaySeries) {
-      rn <- rownames(df)
-      df <- rbind(df, data.frame(
-                         x=  MSE(get_final()[(ind):(ind + 364), "x"],   get_ssa()[1:365, "x"], 365),
-                         y=  MSE(get_final()[(ind):(ind + 364), "y"],   get_ssa()[1:365, "y"], 365),
-                         LOD=MSE(get_final()[(ind):(ind + 364), "LOD"], get_ssa()[1:365, "LOD"], 365),
-                         dX= MSE(get_final()[(ind):(ind + 364), "dX"],  get_ssa()[1:365, "dX"], 365),
-                         dY= MSE(get_final()[(ind):(ind + 364), "dY"],  get_ssa()[1:365, "dY"], 365)))
-      rownames(df) <- c(rn, "SSA")
-    }
-    if("pul_am" %in% input$displaySeries) {
-      rn <- rownames(df)
-      df <- rbind(df, data.frame(
-                         x=  MSE(get_final()[(ind):(ind + 364), "x"],   get_pul_am()[1:365, "x"], 365),
-                         y=  MSE(get_final()[(ind):(ind + 364), "y"],   get_pul_am()[1:365, "y"], 365),
-                         LOD=MSE(get_final()[(ind):(ind + 364), "LOD"], get_pul_am()[1:365, "LOD"], 365),
-                         dX= MSE(get_final()[(ind):(ind + 364), "dX"],  get_pul_am()[1:365, "dX"], 365),
-                         dY= MSE(get_final()[(ind):(ind + 364), "dY"],  get_pul_am()[1:365, "dY"], 365)))
-      rownames(df) <- c(rn, "Pul AM")
-    }
-    if("pul_e1" %in% input$displaySeries) {
-      rn <- rownames(df)
-      df <- rbind(df, data.frame(
-                         x=  MSE(get_final()[(ind):(ind + 364), "x"],   get_pul_e1()[1:365, "x"], 365),
-                         y=  MSE(get_final()[(ind):(ind + 364), "y"],   get_pul_e1()[1:365, "y"], 365),
-                         LOD=MSE(get_final()[(ind):(ind + 364), "LOD"], get_pul_e1()[1:365, "LOD"], 365),
-                         dX= MSE(get_final()[(ind):(ind + 364), "dX"],  get_pul_e1()[1:365, "dX"], 365),
-                         dY= MSE(get_final()[(ind):(ind + 364), "dY"],  get_pul_e1()[1:365, "dY"], 365)))
-      rownames(df) <- c(rn, "Pul E1")
+    n <- length(series.list)
+    for(i in 1:n) {
+      if(series.list[i] %in% input$displaySeries) {
+        rn <- rownames(df)
+        df <- rbind(df, data.frame(
+          x=  MSE(get_final()[(ind):(ind + 364), "x"],   series.getters[[i]]()[1:365, "x"], 365),
+          y=  MSE(get_final()[(ind):(ind + 364), "y"],   series.getters[[i]]()[1:365, "y"], 365),
+          LOD=MSE(get_final()[(ind):(ind + 364), "LOD"], series.getters[[i]]()[1:365, "LOD"], 365),
+          dX= MSE(get_final()[(ind):(ind + 364), "dX"],  series.getters[[i]]()[1:365, "dX"], 365),
+          dY= MSE(get_final()[(ind):(ind + 364), "dY"],  series.getters[[i]]()[1:365, "dY"], 365)))
+        rownames(df) <- c(rn, series.names[i])
+      }
     }
     df
   }, include.rownames=TRUE, digits=10)
@@ -470,18 +245,18 @@ ui = tagList(
              ),
              mainPanel(
                h4("MSE"),
-               tableOutput('comparison_table'),
+               tableOutput("comparison_table"),
                tags$hr(),
                h4("Pole x"),
                plotOutput("x_comparison"),
                h4("Pole y"),
                plotOutput("y_comparison"),
                h4("LOD"),
-               plotOutput("lod_comparison"),
+               plotOutput("LOD_comparison"),
                h4("dX"),
-               plotOutput("dx_comparison"),
+               plotOutput("dX_comparison"),
                h4("dY"),
-               plotOutput("dy_comparison")
+               plotOutput("dY_comparison")
              )
     ),
     tabPanel("About",
